2e755e0fb842de2ecc7acda64b38762dabab0522
Geona Kim (MacOS) <rtguna@icloud.com>
2024-04-12 14:40:30 +0900

    build : 기본 GUI 라이브러리 변경.
    기본 GUI 라이브러리를 GTK4에서 GTKmm4로 변경함.
    GTKmm4가 개발 기간 단축과, 객체지향 프로그래밍에 더 적합하다고 판단이 됨.
    
    ~~~~
    
    build: Changed default GUI library.
    
    Changed the default GUI library from GTK4 to GTKmm4.
    The decision was made considering the benefits of GTKmm4, including shortened development time and better suitability for object-oriented programming.

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,16 @@
-#include <gtk/gtk.h>
+#include <gtkmm.h>
 
-static void print_hello (GtkWidget *widget, gpointer data)
+class MyWindow : public Gtk::Window
 {
-    g_print ("Hello World\n");
-}
+public:
+    MyWindow() {
+        set_title("window");
+        set_default_size(200, 200);
+    }
+};
 
-static void activate (GtkApplication* app, gpointer user_data)
+int main(int argc, char* argv[])
 {
-    GtkWidget *window;
-    GtkWidget *button;
-    GtkWidget *box;
-
-    window = gtk_application_window_new (app);
-    gtk_window_set_title (GTK_WINDOW (window), "Window");
-    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-
-    box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
-    gtk_widget_set_halign (box, GTK_ALIGN_CENTER);
-    gtk_widget_set_valign (box, GTK_ALIGN_CENTER);
-
-    gtk_window_set_child (GTK_WINDOW (window), box);
-
-    button = gtk_button_new_with_label ("Hello World");
-
-    g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-    g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_window_destroy), window);
-
-    gtk_box_append (GTK_BOX (box), button);
-
-    gtk_window_present (GTK_WINDOW (window));
+    auto app = Gtk::Application::create("org.gtkmm.examples.base");
+    return app->make_window_and_run<MyWindow>(argc, argv);
 }
-
-int main (int argc, char** argv)
-{
-    GtkApplication *app;
-    int status;
-
-    app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-    status = g_application_run (G_APPLICATION (app), argc, argv);
-    g_object_unref (app);
-
-    return status;
-}
-
