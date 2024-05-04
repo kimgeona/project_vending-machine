a7fbbbb4bc7db1521dea825f79e23f5eb16b7de0
Geona Kim (MacOS) <rtguna@icloud.com>
2024-05-04 21:54:49 +0900

    test : [데이터 관리 모듈] 코드 작성.
    data_DataManagement.hpp
    data_DataManagement.cpp
    : [불러오기], [저장] 을 구현함.
    : GUI 어플리케이션에서 이벤트 호출에 사용될 함수들 원형 몇개 작성해봄.
    
    ~~~~
    
    test: [Data Management Module] Code written.
    
    data_DataManagement.hpp
    data_DataManagement.cpp
    : Implemented [Load] and [Save].
    : Defined several function prototypes to be used for event handling in the GUI application.

--- include/data_DataManagement.hpp
+++ include/data_DataManagement.hpp
@@ -1,15 +1,18 @@
 #pragma once
 #ifndef data_DataManagement_hpp
-#define data_DataManagement_h
+#define data_DataManagement_hpp
 
 // c++17
+#include <iostream>
 #include <string>
-#include <chorono>
+#include <filesystem>
+#include <fstream>
 
 // 나의 라이브러리
 #include <data_Drink.hpp>
-#include <data_Cash.hpp>
+#include <data_Coin.hpp>
 #include <algo.hpp>
+#include <util.hpp>
 
 namespace data
 {
@@ -17,40 +20,52 @@ namespace data
 
 class DataManagement
 {
+private:
+    // 자판기 데이터 경로
+    std::filesystem::path dir;
+    
+    // 사용자 계정
+    std::string             ID;             // 계정 ID
+    std::string             PW;             // 계정 PW
+    
+    // 재고
+    algo::LinkedList<Drink> drinks[6];      // 자판기 음료
+    algo::Stack<Coin>       coins[5];       // 자판기 거스름돈
+    
+    // 버퍼
+    algo::Queue<Drink>      vm_out_drinks;  // 음료 출구
+    algo::Queue<Coin>       vm_out_coins;   // 거스름돈 출구
+    algo::Queue<Coin>       vm_in_coins;    // 동전 입구
+    
+    // 데이터 불러오기 및 저장
+    void load();
+    void save();
+    
 public:
-    // 서버
-    // 연결된 서버 정보
+    // 생성자
+    DataManagement();
+    DataManagement(std::string);
     
-    // 상태
-    // 현재 날짜 변수
-    // 현재 시간 변수
-    // -> 서버 동기화에 쓰임
+    // MainPage
+    void select_drink(int n);               // 음료 선택
+    void select_cash(int n);                // 금액 입력
+    void take_drink(void);                  // 음료 가져가기
+    void take_change(void);                 // 거스름돈 가져가기
+    void purchase(void);                    // 구매
+    void open_administrator_page(void);     // 관리자 메뉴 열기
     
-    // 자판기 계정
-    std::string ID;
-    std::string PW;
+    // LoginPage
+    bool login(std::string id, std::string pw); // 로그인
     
-    // 자판기 음료 재고
-    algo::LinkedList<Drink> drink_1;
-    algo::LinkedList<Drink> drink_2;
-    algo::LinkedList<Drink> drink_3;
-    algo::LinkedList<Drink> drink_4;
-    algo::LinkedList<Drink> drink_5;
-    algo::LinkedList<Drink> drink_6;
-    
-    // 자판기 거스름돈 재고
-    algo::Stack<Cash> cash_10;
-    algo::Stack<Cash> cash_50;
-    algo::Stack<Cash> cash_100;
-    algo::Stack<Cash> cash_500;
-    algo::Stack<Cash> cash_1000;
-    
-    // 자판기 음료 출구
-    algo::Queue<Drink> vm_out_drinks;
-    
-    // 자판기 거스름돈 출구
-    algo::Queue<Coin> vm_out_coins;
-}
+    // AdministratorPage
+    std::string modify_id(std::string);                     // 사용자 이름 변경
+    std::string modify_pw(std::string);                     // 비밀번호 변경
+    std::string modify_drink_name(int n, std::string);      // 음료 이름 변경
+    std::string modify_drink_price(int n, std::string);     // 음료 가격 수정
+    std::string modify_drink_stock(int n, std::string);     // 음료 갯수 변경
+    std::string modify_cash_changes(int n, std::string);    // 잔돈 갯수 변경
+    void        collect_changes(void);                      // 잔돈 수금
+};
 
 
 }
--- lib/data_DataManagement.cpp
+++ lib/data_DataManagement.cpp
@@ -4,7 +4,145 @@ namespace data
 {
 
 
+DataManagement::DataManagement(std::string backup_dir)
+{
+    using namespace std;
+    using namespace std::filesystem;
+    
+    // 자판기 데이터 경로 저장
+    dir = path(backup_dir);
+    
+    // 경로 확인
+    if (exists(dir) && is_regular_file(dir))
+    {
+        cout << "|  data::DataManagement : 자판기 데이터를 불러옵니다." << endl;
+        
+        // 데이터 불러오기
+        load();
+    }
+    else {
+        cout << "|  data::DataManagement : 자판기를 새로 생성합니다." << endl;
+        
+        // 사용자 계정 생성
+        ID = "admin";
+        PW = "admin";
+        
+        // 음료 생성
+        for (int i=0; i<10; i++) drinks[0].push_back(Drink("물", 450));
+        for (int i=0; i<10; i++) drinks[1].push_back(Drink("커피", 500));
+        for (int i=0; i<10; i++) drinks[2].push_back(Drink("이온음료", 550));
+        for (int i=0; i<10; i++) drinks[3].push_back(Drink("고급커피", 700));
+        for (int i=0; i<10; i++) drinks[4].push_back(Drink("탄산음료", 750));
+        for (int i=0; i<10; i++) drinks[5].push_back(Drink("특화음료", 800));
+        
+        // 잔돈 생성
+        for (int i=0; i<10; i++) coins[0].push(Coin(10, 2024));
+        for (int i=0; i<10; i++) coins[1].push(Coin(50, 2024));
+        for (int i=0; i<10; i++) coins[2].push(Coin(100, 2024));
+        for (int i=0; i<10; i++) coins[3].push(Coin(500, 2024));
+        for (int i=0; i<10; i++) coins[4].push(Coin(1000, 2024));
+        
+        // 데이터 저장
+        save();
+    }
+    cout << "|  data::DataManagement : 완료." << endl;
+}
 
+void DataManagement::load()
+{
+    using namespace std;
+    using namespace std::filesystem;
+    
+    // 변수 생성
+    string buf;
+    
+    // 파일 열기
+    ifstream fin(dir.string());
+    if (!fin)
+    {
+        throw std::runtime_error("data::DataManagement::load() : 자판기 데이터 파일을 열 수 없습니다.");
+    }
+    
+    while (getline(fin, buf))   // 엔터 제거해서 받음
+    {
+        // 읽어들인 문자열 전처리
+        buf = util::my_strip(buf);                          // 좌우 공백 제거
+        vector<string> words = util::my_split(buf, "=");    // "="을 기준으로 split
+        
+        // 전처리된 문자열이 없으면 건너뛰기
+        if (words.empty()) continue;
+        
+        // 사용자 계정 정보 읽기
+        if (words[0]=="ID")
+        {
+            ID = words[1];
+            continue;
+        }
+        if (words[0]=="PW")
+        {
+            PW = words[1];
+            continue;
+        }
+        
+        // 음료 정보 읽기
+        if (words[0].find("drink")==0)
+        {
+            vector<string> type = util::my_split(words[0], "-");
+            vector<string> data = util::my_split(words[1], "-");
+            
+            drinks[stoi(type[1])].push_back(Drink(data[0], stoi(data[1])));
+            
+            continue;
+        }
+        
+        // 잔돈 정보 읽기
+        if (words[0].find("coin")==0)
+        {
+            vector<string> type = util::my_split(words[0], "-");
+            vector<string> data = util::my_split(words[1], "-");
+            
+            coins[stoi(type[1])].push(Coin(stoi(data[0]), stoi(data[1])));
+            
+            continue;
+        }
+    }
+    
+    // 파일 스트림 닫기
+    fin.close();
+    
+    // 읽어들인 파일 저장
+    save();
+}
+void DataManagement::save()
+{
+    using namespace std;
+    using namespace std::filesystem;
+    
+    // 기존에 존재하던 백업 파일 삭제
+    if (exists(dir) && is_regular_file(dir)) remove(dir);
+    
+    // 파일 생성
+    ofstream fout(dir.string());
+    
+    // 사용자 계정 정보 저장
+    fout << "ID=" << ID << endl;
+    fout << "PW=" << PW << endl << endl;
+    
+    // 음료 정보 저장
+    for (int i=0; i<6; i++)
+        for (int j=0; j<drinks[i].size(); j++)
+            fout << "drink-" << i << "-" << j << "=" << drinks[i][j].name << "-" << drinks[i][j].price << endl;
+    fout << endl;
+    
+    // 잔돈 정보 저장
+    for (int i=0; i<5; i++)
+        for (int j=0; j<coins[i].size(); j++)
+            fout << "coin-" << i << "-" << j << "=" << coins[i][j].amount << "-" << coins[i][j].banknote << endl;
+    fout << endl;
+    
+    // 파일 스트림 닫기
+    fout.close();
+}
 
 
 }
