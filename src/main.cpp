9c11a3290b314d5051d52503acaf36a61757a05e
Geona Kim (MacOS) <rtguna@icloud.com>
2024-05-05 20:57:44 +0900

    test : [메인 프로그램] 코드 작성.
    ~~~~
    
    test: [Main Program] Code written.

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,57 @@
 
 // 나의 라이브러리
 #include <gui.hpp>
+#include <data.hpp>
+
+
+void preparation();                 // 프로그램 준비
+void run(int argc, char* argv[]);   // 프로그램 시작
+void test();                        // 테스트
+
 
 int main(int argc, char* argv[])
 {
-    int e1=0, e2=0, e3=0;
+    // 1. 프로그램 준비
+    preparation();
     
-    // 자판기 어플리케이션 메인 페이지 실행
+    // 2. 프로그램 시작
+    run(argc, argv);
+    
+    // 0. 테스트
+    //test();
+    
+    return 0;
+}
+
+
+void preparation()
+{
+    using namespace std;
+    cout << "Vending-Machine : 프로그램을 준비합니다." << endl;
+}
+
+
+void run(int argc, char* argv[])
+{
+    using namespace std;
+    cout << "Vending-Machine : GUI 어플리케이션을 실행합니다." << endl;
+    
     auto app_MainPage = Gtk::Application::create("kimgeona.VendingMachine.MainPage");
-    e1 = app_MainPage->make_window_and_run<gui::MainPage>(argc, argv);
-    
-    // 자판기 어플리케이션 로그인 페이지 실행
     auto app_LoginPage = Gtk::Application::create("kimgeona.VendingMachine.LoginPage");
-    e2 = app_LoginPage->make_window_and_run<gui::LoginPage>(argc, argv);
-    
-    // 자판기 어플리케이션 관리자 페이지 실행
     auto app_AdministratorPage = Gtk::Application::create("kimgeona.VendingMachine.AdministratorPage");
-    e3 = app_AdministratorPage->make_window_and_run<gui::AdministratorPage>(argc, argv);
     
-    return e1 || e2 || e3;
+    app_MainPage->make_window_and_run<gui::MainPage>(argc, argv)    ||
+    app_LoginPage->make_window_and_run<gui::LoginPage>(argc, argv)  ||
+    app_AdministratorPage->make_window_and_run<gui::AdministratorPage>(argc, argv);
+    
+    cout << "Vending-Machine : 프로그램을 종료합니다." << endl;
+}
+
+
+void test()
+{
+    using namespace std;
+    cout << "Test : 데이터 관리자를 실행합니다." << endl;
+    
+    data::DataManagement dm("test.txt");
 }
