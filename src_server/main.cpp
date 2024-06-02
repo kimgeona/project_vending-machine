8f97dae5185bd2ead448137e8a9a32956da2f1a7
Geona Kim (MacOS) <rtguna@icloud.com>
2024-06-02 10:44:27 +0900

    build : client 프로그램과 server 프로그램을 따로 개발하기 위한 cmake 준비.
    client 프로그램과 server 프로그램을 따로 작성, 개발하기위해 각각의 디렉토리를 만들고 cmake 파일을 수정함.
    
    ~~~~
    
    build: Prepared cmake for separate development of client and server programs.
    
    Created separate directories for writing and developing client and server programs and modified the cmake files accordingly.

---
+++ src_server/main.cpp
@@ -0,0 +1,17 @@
+// c++17
+#include <gtkmm.h>
+#include <iostream>
+#include <map>
+
+// 나의 라이브러리
+#include <gui.hpp>
+#include <data.hpp>
+
+int main(int argc, char* argv[])
+{
+    using namespace std;
+    
+    cout << "서버 프로그램입니다." << endl;
+
+    return 0;
+}
