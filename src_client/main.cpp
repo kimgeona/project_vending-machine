5b144f49d5b2ecee36b29fbd4f58cf94910efa65
Geona Kim (MacOS) <rtguna@icloud.com>
2024-06-09 01:23:21 +0900

    chore : [메인 프로그램] 클라이언트 메인 프로그램 작성 완료.
    src_client/main.cpp
    : 프로그램 실행시 어떤 자판기를 열지 선택할 수 있음.
    : 이미 열려있는 파일은 열지 않도록 코드를 작성함.
    
    ~~~~
    
    chore: [Main Program] Complete writing the client main program.
    
    src_client/main.cpp
    : Allow selection of which vending machine to open when the program starts.
    : Write code to prevent opening files that are already open.

--- src_client/main.cpp
+++ src_client/main.cpp
@@ -9,31 +9,52 @@
 #include <network.hpp>
 #include <gui_client.hpp>
 
+// 프로그램 버전
+#define VM_VERSION (std::to_string(VM_VERSION_MAJOR) + "."+ std::to_string(VM_VERSION_MINOR) + "." + std::to_string(VM_VERSION_PATCH))
+
 // 전역 변수
 data::DataManagement                    dm;
 std::map<std::string, Gtk::Widget*>     widget;
 
 // 통신 모듈
-network::Pipe       pipe_to_server("client", "test");
+network::Pipe       pipe_to_server("client", "name");
 Glib::Dispatcher    dispatcher;
 
 int main(int argc, char* argv[])
 {
-    // 1. 프로그램 준비
-    std::cout << "Vending-Machine : 데이터 관리자를 생성합니다." << std::endl;
-    dm = data::DataManagement("test");
-    dm.state = "on";
-    dm.save();
+    // 1. 자판기 이름 입력받기
+    std::string vm_name = "";
+    std::cout << "|  ⸝⸝• ̫•⸝⸝"                                          << std::endl;
+    std::cout << "|"                                                << std::endl;
+    std::cout << "|  Vending-Machine client(" << VM_VERSION << ")"  << std::endl;
+    std::cout << "|-"                                               << std::endl;
+    while (true)
+    {
+        std::cout << "|  자판기 이름 입력 : ";
+        std::cin >> vm_name;
+        std::cout << "|" << std::endl << "|" << std::endl;
+        
+        // 2. 사용중이지 않은 자판기인지 확인하고 불러오기
+        dm = data::DataManagement(vm_name);
+        if (dm.state == "off")
+        {
+            dm.state = "on";
+            dm.save();
+            break;
+        }
+        std::cout << "|  이미 실행중인 자판기 입니다." << std::endl;
+    }
     
-    // 2. 서버와 연결
+    // 3. 서버와 연결
+    pipe_to_server.change_name(vm_name);
     pipe_to_server.start();
     
-    // 3. 프로그램 실행
-    std::cout << "Vending-Machine : GUI 어플리케이션을 실행합니다." << std::endl;
+    // 4. 프로그램 실행
+    std::cout << "|-\n|  GUI 어플리케이션을 실행합니다.\n|-\n";
     auto n = Gtk::Application::create("kimgeona.VendingMachine.client")->make_window_and_run<gui_client::MainPage>(argc, argv);
-    std::cout << "Vending-Machine : GUI 어플리케이션을 종료합니다." << std::endl;
+    std::cout << "|-\n|  GUI 어플리케이션을 종료합니다.\n|-\n";
     
-    // 4. 프로그램 종료
+    // 5. 프로그램 종료
     pipe_to_server.end();
     
     return n;
