549d4132c52d8045e0a128820f3d500ef56e1d9b
Geona Kim (MacOS) <rtguna@icloud.com>
2024-06-09 01:28:48 +0900

    chore : [메인 프로그램] 서버  메인 프로그램 작성 완료.
    src_server/main.cpp
    : 해당 서버 프로그램의 버전 정보를 출력하고, 출력하는 텍스트들을 조금 더 보기 좋게 수정함.
    
    ~~~~
    
    chore: [Main Program] Complete writing the server main program.
    
    src_server/main.cpp
    : Print the version information of the server program and modify the output texts to be more readable.

--- src_server/main.cpp
+++ src_server/main.cpp
@@ -9,6 +9,9 @@
 #include <network.hpp>
 #include <gui_server.hpp>
 
+// 프로그램 버전
+#define VM_VERSION (std::to_string(VM_VERSION_MAJOR) + "."+ std::to_string(VM_VERSION_MINOR) + "." + std::to_string(VM_VERSION_PATCH))
+
 // 전역 변수
 std::map<std::string, data::DataManagement> dms;
 std::map<std::string, Gtk::Widget*>         widget;
@@ -21,13 +24,19 @@ int main(int argc, char* argv[])
 {
     using namespace std;
     
-    // 1. 클라이언트와 연결
+    // 1. 기본 내용 출력
+    std::cout << "|  ⸝⸝• ̫•⸝⸝"                                          << std::endl;
+    std::cout << "|"                                                << std::endl;
+    std::cout << "|  Vending-Machine server(" << VM_VERSION << ")"  << std::endl;
+    std::cout << "|-"                                               << std::endl;
+    
+    // 2. 클라이언트와 연결
     pipe_to_client.start();
     
     // 2. 프로그램 실행
-    cout << "Vending-Machine-server : GUI-server 어플리케이션을 실행합니다." << endl;
+    std::cout << "|-\n|  GUI 어플리케이션을 실행합니다.\n|-\n";
     auto n = Gtk::Application::create("kimgeona.VendingMachine.server")->make_window_and_run<gui_server::ListPage>(argc, argv);
-    cout << "Vending-Machine-server : GUI-server 어플리케이션을 종료합니다." << endl;
+    std::cout << "|-\n|  GUI 어플리케이션을 종료합니다.\n|-\n";
     
     // 3. 프로그램 종료
     pipe_to_client.end();
